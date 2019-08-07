9c522659c6ec9c51af5cd91c1c902edb92b3b537
Sam DeKlyen <samdeklyen@gmail.com>
2019-08-07 16:12:59 -0400

    day 3

--- 5-expressionsAndOperators/03-ternaries.js
+++
@@ -1,5 +0,0 @@
-// TERNARY
-
-
-let age = 14;
-(age >=25) ? console.log('Yay! You can rent a car') : (age>=21) ? console.log('Yay! You can drink!') : (age >=18) ? console.log('Yay! You can vote') : (age<18) ? console.log('Sorry you are too young to do anything') : console.log('That is not a number');
